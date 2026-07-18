8be2b77a541b144a579f61a799f619df33047192
webdevavi96 <avinashchaurasiya902@gmail.com>
2026-07-18 16:49:18 +0530

    some issues fixed

--- ChatApp/Server/src/config/conn.db.js
+++ ChatApp/Server/src/config/conn.db.js
@@ -5,7 +5,7 @@ const db_uri = process.env.MONGO_DB_PROD_URI || `${db_conn}/${db_name}`
 
 export const connect = async () => {
   try {
-    const connectionInstance = await mongoose.connect(`${db_uri}`);
+    const connectionInstance = await mongoose.connect(db_uri);
     console.log("database connceted successfully");
   } catch (error) {
     console.error(error);

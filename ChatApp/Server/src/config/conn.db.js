6365559825f81d5522d595e62af6bb72afabbb64
webdevavi96 <avinashchaurasiya902@gmail.com>
2026-07-18 16:44:30 +0530

    some issues fixed

--- ChatApp/Server/src/config/conn.db.js
+++ ChatApp/Server/src/config/conn.db.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 import { db_conn, db_name } from "../../constant.js";
 
+const db_uri = process.env.MONGO_DB_PROD_URI || `${db_conn}/${db_name}`
+
 export const connect = async () => {
   try {
-    const connectionInstance = await mongoose.connect(`${db_conn}/${db_name}`);
+    const connectionInstance = await mongoose.connect(`${db_uri}`);
     console.log("database connceted successfully");
   } catch (error) {
     console.error(error);
